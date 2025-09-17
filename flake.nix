9aee4a3d8b0ecef6b7763a2f7d167ae34933ccbe
janusz-bit <antyphone@gmail.com>
2025-09-17 10:20:54 +0200

    Cleaning code

--- flake.nix
+++ flake.nix
@@ -17,14 +17,6 @@
         system = linux;
         config.allowUnfree = true;
       };
-      pkgsCross = import nixpkgs {
-        system = linux;
-        config.allowUnfree = true;
-        crossSystem = {
-          config = "x86_64-w64-mingw32";
-          libc = "msvcrt"; # This distinguishes the mingw (non posix) toolchain
-        };
-      };
 
     in
     {
@@ -47,56 +39,15 @@
             ninja
             # sdl3
             gdb
-            libGL
-            libayatana-appindicator
-            libdecor
-            libdrm
-            libjack2
-            libpulseaudio
-            libusb1
-            libxkbcommon
-            libgbm
-            ninja
-            pipewire
-            sndio
-            systemdLibs
-            vulkan-headers
-            vulkan-loader
-            wayland
-            wayland-scanner
-            zenity
-            doxygen
-            doxygen_gui
-          ];
-        };
-        windows = pkgsCross.mkShell {
 
-          name = "sdl-dev-shell";
-          shellHook = ''
-            echo "Welcome to the development environment!"
-            HOME=$PWD/replacement-home
-            echo "${pkgsCross.stdenv.hostPlatform.emulator pkgsCross.buildPackages}"
-          '';
-          nativeBuildInputs = with pkgs; [
-            cmake
-            pkg-config
-            gcc
-            clang-tools
-            # gnumake
-            ninja
-            wine
-            wine64
-            winetricks
-            gdb
-          ];
-
-          buildInputs = with pkgsCross; [
-            sdl3
           ];
         };
       };
 
-      templates.default = { path = ./.; description = "C++ template"; };
+      templates.default = {
+        path = ./.;
+        description = "C++ template";
+      };
 
     };
 }
